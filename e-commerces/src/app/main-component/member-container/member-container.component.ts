6165eca68dce1078f711cd8a4ec950aa40d62caf
Trinh Xuan Le <letx.1502@gmail.com>
2022-07-31 21:43:41 +0700

    create home structure

---
+++ e-commerces/src/app/main-component/member-container/member-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-container',
+  templateUrl: './member-container.component.html',
+  styleUrls: ['./member-container.component.scss']
+})
+export class MemberContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
